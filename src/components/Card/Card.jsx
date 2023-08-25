a4f210f949464488e6843c346e26d5ac322f8b25
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-08-25 13:49:29 +0530

    responsive spacing and font-sizes added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,43 +2,57 @@ import cardImageDesktop from "../../assets/image-header-desktop.jpg";
 import cardImageMobile from "../../assets/image-header-mobile.jpg";
 import "./Card.css";
 
+const stats = [
+  {
+    title: "10k+",
+    label: "Companies",
+  },
+  {
+    title: "314",
+    label: "Templates",
+  },
+  {
+    title: "12M+",
+    label: "Queries",
+  },
+];
+
 function Card() {
   return (
-    <div className="card">
-      <div className="card__image-wrapper">
-        <picture className="card__picture">
-          <source media="(min-width: 48em)" srcSet={cardImageDesktop} />
-          <img
-            className="card__image"
-            src={cardImageMobile}
-            alt="Three womens doing some work"
-          />
-        </picture>
-      </div>
-      <div className="card__content">
-        <h2 className="card__title">
-          Get insights that help your business grow.
-        </h2>
-        <p className="card__description">
-          Discover the benefits of data analytics and make better decisions
-          regarding revenue, customer experience, and overall efficiency.
-        </p>
-        <div className="card__stats">
-          <div className="card__stat">
-            <h3 className="card__stat-title">10k+</h3>
-            <p className="card__stat-label">Companies</p>
-          </div>
-          <div className="card__stat">
-            <h3 className="card__stat-title">314</h3>
-            <p className="card__stat-label">Templates</p>
-          </div>
-          <div className="card__stat">
-            <h3 className="card__stat-title">12M+</h3>
-            <p className="card__stat-label">Queries</p>
+    <section className="card-section">
+      <div className="card grid grid-cols-2">
+        <div className="card__image-wrapper">
+          <picture className="card__picture">
+            <source media="(min-width: 48em)" srcSet={cardImageDesktop} />
+            <img
+              className="card__image"
+              src={cardImageMobile}
+              alt="Three womens doing some work"
+            />
+          </picture>
+        </div>
+        <div className="card__content flow">
+          <h2 className="fs-400">
+            Get <span className="clr-violet">insights</span> that help your
+            business grow.
+          </h2>
+          <p>
+            Discover the benefits of data analytics and make better decisions
+            regarding revenue, customer experience, and overall efficiency.
+          </p>
+          <div className="grid grid-cols-3 gap">
+            {stats.map((stat, index) => (
+              <div key={index}>
+                <h3 className="fs-300 ">{stat.title}</h3>
+                <span className="card__stat-label fs-100 clr-grey text-uppercase">
+                  {stat.label}
+                </span>
+              </div>
+            ))}
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
